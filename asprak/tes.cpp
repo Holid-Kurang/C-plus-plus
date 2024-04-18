98c99edc1a5d84c4d42298d02dff602b38b59f3b
Holid-Kurang <kh5lilulloh@gmail.com>
2024-04-19 02:54:53 +0800

    orginize folder

--- asprak/tes.cpp
+++ asprak/tes.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void display(int m) {
-	m+=10;
-    // display array elements    
-}
-
 int main() {
+    int var = 5;
 
-    // declare and initialize an array
-    int marks = 5;
+    // store address of var
+    int* point_var ;
+    point_var = &var;
+
+    // print value of var
+    cout << "var = " << var << endl;
+
+    cout << "Address of var (&var) = " << &var << endl
+         << endl;
+
+    // print pointer point_var
+    cout << "point_var = " << point_var << endl;
+
+    // print the content of the address point_var points to
+    cout << "Content of the address pointed to by point_var (*point_var) = " << *point_var << endl;
     
-    // call display function
-    // pass array as argument
-    display(marks);
-
-	// display array elements after modification
-	cout << marks << endl;
+    return 0;
 	return 0;
 }
