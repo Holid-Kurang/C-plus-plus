5dbb96d19671c424da7255969817a54ae86f004e
Holid-Kurang <kh5lilulloh@gmail.com>
2024-05-05 12:49:44 +0800

    pezzle geser improvement

--- asprak/puzzleGeser.cpp
+++ asprak/puzzleGeser.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <conio.h>
-#include <iomanip>
 using namespace std;
 
-void printpuzzle(int puzzle[3][4]) {
+void printpuzzle(int puzzle[4][4]) {
 	system("cls");
-	for (int y = 0; y < 3; y++) {
+	for (int y = 0; y < 4; y++) {
 		for (int x = 0; x < 4; x++) {
 			cout << setw(2);
-			if (puzzle[y][x] == 12) {
-				cout << " -" << puzzle[y][x] << "- ";
+			if (puzzle[y][x] == 16) {
+				cout << " <" << puzzle[y][x] << "> ";
 			}
 			else {
 				if (puzzle[y][x] < 10) {
@@ -21,38 +20,31 @@ void printpuzzle(int puzzle[3][4]) {
 			}
 		}
 		cout << endl;
-	}cout << "tekan enter kalo nyerah!!\n";
-}
-void lokasi(int puzzle[3][4], int& pX, int& pY) {
-	for (int y = 0; y < 3; y++) {
-		for (int x = 0; x < 4; x++) {
-			if (puzzle[y][x] == 12) {
-				pY = y, pX = x;
-			}
-		}
 	}
+	cout << "tekan enter kalo nyerah!!"<<endl;
+	cout << "tekan r/esc untuk restart"<<endl;
 }
-int lokasiX(int puzzle[3][4]) {
-	for (int y = 0; y < 3; y++) {
+int lokasiX(int puzzle[4][4]) {
+	for (int y = 0; y < 4; y++) {
 		for (int x = 0; x < 4; x++) {
-			if (puzzle[y][x] == 12) {
+			if (puzzle[y][x] == 16) {
 				return x;
 			}
 		}
 	}
 	return 0;
 }
-int lokasiY(int puzzle[3][4]) {
-	for (int y = 0; y < 3; y++) {
+int lokasiY(int puzzle[4][4]) {
+	for (int y = 0; y < 4; y++) {
 		for (int x = 0; x < 4; x++) {
-			if (puzzle[y][x] == 12) {
+			if (puzzle[y][x] == 16) {
 				return y;
 			}
 		}
 	}
 	return 0;
 }
-void gerak(int puzzle[3][4], char slide, int kX, int kY) {
+void gerak(int puzzle[4][4], char slide, int kX, int kY) {
 	int temp, stop;
 	if (slide == 72) { //atas
 		stop = kY - 1;
@@ -77,27 +69,35 @@ void gerak(int puzzle[3][4], char slide, int kX, int kY) {
 	}
 	else if (slide == 80) { //bawah
 		stop = kY + 1;
-		if (stop > 2) stop = 2;
+		if (stop > 3) stop = 3;
 		temp = puzzle[stop][kX];
 		puzzle[stop][kX] = puzzle[kY][kX];
 		puzzle[kY][kX] = temp;
 	}
 }
-bool checkWin(int puzzle[3][4]) {
-	if (puzzle[0][0] == 1 && puzzle[0][1] == 2 && puzzle[0][2] == 3 && puzzle[0][3] == 4 && 
-		puzzle[1][0] == 5 && puzzle[1][1] == 6 && puzzle[1][2] == 7 && puzzle[1][3] == 8 && 
-		puzzle[2][0] == 9 && puzzle[2][1] ==10 && puzzle[2][2] ==11 && puzzle[2][3] ==12 ) {
+bool checkWin(int puzzle[4][4]) {
+	if (puzzle[0][0] == 1 && puzzle[0][1] == 2 && puzzle[0][2] == 3 && puzzle[0][3] == 4 &&
+		puzzle[1][0] == 5 && puzzle[1][1] == 6 && puzzle[1][2] == 7 && puzzle[1][3] == 8 &&
+		puzzle[2][0] == 9 && puzzle[2][1] ==10 && puzzle[2][2] ==11 && puzzle[2][3] ==12 &&
+		puzzle[3][0] ==13 && puzzle[3][1] ==14 && puzzle[3][2] ==15 && puzzle[3][3] ==16 ) {
 		return true;
 	}
 	return false;
 }
+void resetPuzzle(int puzzle[4][4]) {
+    int resetValues[4][4] = { {1,8,4,15},{2,5,16,10},{9,6,3,11},{14,12,13,7} };
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4; j++) {
+            puzzle[i][j] = resetValues[i][j];
+        }
+    }
+}
 int main() {
 	char a;
-	int puzzle[3][4] = { {1,8,3,10},{7,5,6,11},{4,9,2,12} };
+	int puzzle[4][4] = { {1,8,4,15},{2,5,16,10},{9,6,3,11},{14,12,13,7} };
 	int kY, kX;
 	while (1) {
 		printpuzzle(puzzle);
-		// lokasi(puzzle, kX, kY);
 		kX = lokasiX(puzzle);
 		kY = lokasiY(puzzle);
 		if (checkWin(puzzle)) {
@@ -105,7 +105,10 @@ int main() {
 			break;
 		}
 		a = getch();
-		if (a == 13) break; //enter
+		if (a == 13) 
+			break; //enter
+		else if (a == 'R' || a == 'r'|| a==27) 
+			resetPuzzle(puzzle); // Restart
 		gerak(puzzle, a, kX, kY);
 	}
 }
