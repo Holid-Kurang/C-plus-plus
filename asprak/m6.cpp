faf7cb1c106d416c4fee7b8bca355e16e6ee2bdb
Holid-Kurang <kh5lilulloh@gmail.com>
2024-05-17 17:28:21 +0800

    m6 prog

--- asprak/m6.cpp
+++ asprak/m6.cpp
@@ -9,11 +9,11 @@ struct Item {
 
     bool isItemEmpty() {
         return name == "";
-    } 
+    }
 };
 
 struct Player {
-    bool dead=false;
+    bool dead = false;
     string name;
     int hp;
     int atk;
@@ -29,12 +29,12 @@ struct Player {
 }player[2];
 
 struct Monster {
-    bool dead=false;
+    bool dead = false;
     string type;
     int hp;
     int atk;
     int def;
-    
+
     bool isMonsterEmpty() {
         return type == "";
     }
@@ -49,29 +49,12 @@ struct Stage {
     Monster* monster;
     Item* item;
     int prevStage = -1;
-    int nextStage[2] = {-1, -1};
+    int nextStage[2] = { -1, -1 };
 
     bool isStageEmpty() {
         return player[0].isPlayerEmpty() && player[1].isPlayerEmpty();
     }
 };
-
-//*           (^)0
-//             |
-//             |
-//            (#)1
-//             |
-//             |
-//            (#)2
-//            /  \_
-//           /     \_
-//  3       /        \
-// (#)----(#)4       (#)5
-//                    |
-//                   /
-//                  |
-//                   \
-//                   (@)6 */
 
 Stage* createDungeon() {
     Stage* stage = new Stage[7];
@@ -81,10 +64,10 @@ Stage* createDungeon() {
     stage[1].roomName = "111";
     stage[1].nextStage[0] = 2;
     stage[1].prevStage = 0;
-    
+
     stage[2].roomName = "222";
     stage[2].nextStage[0] = 4;
-    stage[2].nextStage[1]= 5;
+    stage[2].nextStage[1] = 5;
 
     stage[4].roomName = "444";
     stage[4].nextStage[0] = 3;
@@ -99,62 +82,94 @@ Stage* createDungeon() {
 
     stage[6].roomName = "666";
     stage[6].prevStage = 5;
-    
-    Monster slime = {true,"Slime", 100, 5, 2};
-    Monster goblin = {true,"Goblin", 200, 10, 5};
-    Monster medusa = {true,"Medusa", 500, 50, 10};
 
-    stage[1].monster = &slime;
-    stage[4].monster = &goblin;
-    stage[6].monster = &medusa;
+    Monster phoneix = { true,"Phoneix", 750, 75, 10 };
+    Monster beastBear = { true,"Beast Bear", 1250, 40, 30 };
+    Monster leviathan = { true,"Leviathan", 1750, 120, 20 };
 
-    Item obatkuat = {"Obat Kuat", "Increase attack by 999", 0, 999};
-    Item obatsehat = {"Obat Sehat", "Increase HP by 999", 999, 0};
-    stage[3].item = &obatkuat;
-    stage[4].item = &obatsehat;
+    stage[1].monster = &beastBear;
+    stage[4].monster = &phoneix;
+    stage[6].monster = &leviathan;
+
+    Item obatKuat = { "Obat Kuat", "Increase attack by 5005", 0, 5005 };
+    Item obatSehat = { "Obat Sehat", "Increase HP by 5005", 5005, 0 };
+    stage[3].item = &obatKuat;
+    stage[4].item = &obatSehat;
 
     player[0].name = "Holil";
-    player[0].hp = 100;
-    player[0].atk = 25;
-    player[0].def = 5;
+    player[0].hp = 1000;
+    player[0].atk = 90;
+    player[0].def = 10;
     player[1].name = "Gilang";
-    player[1].hp = 100;
-    player[1].atk = 20;
-    player[1].def = 5;
+    player[1].hp = 1000;
+    player[1].atk = 80;
+    player[1].def = 15;
 
     stage[0].player[0] = player[0];
     stage[0].player[1] = player[1];
-    
+
     return stage;
+}
+void beastBearModel() {
+    cout << "   ..------~~~--.__" << endl;
+    cout << "  /               c~\\" << endl;
+    cout << "  /             \\__ `\\" << endl;
+    cout << "  |  /~~--__/  /'\\ ~~'" << endl;
+    cout << " /'/'\\ |    | |`\\ \\_" << endl;
+    cout << "`-))  `-))  `-)) `-))" << endl;
+}
+void phoneixModel() {
+    cout << "    ///,        ////" << endl;
+    cout << "    \\ `/,      /` .>." << endl;
+    cout << "     \\  /,   _/` /." << endl;
+    cout << "      \\_ `/_/`  /." << endl;
+    cout << "       \\__/_  .<" << endl;
+    cout << "       /<<<<\\_\\_" << endl;
+    cout << "      /,)^>>_._\\\\" << endl;
+    cout << "      (/   \\\\ /\\\\\\" << endl;
+    cout << "           //  ```` " << endl;
+    cout << "          ((``" << endl;
+}
+void levithanModel() {
+    cout << "       ,_____\\.--.,.--./" << endl;
+    cout << "    ((__@@/  /         \\" << endl;
+    cout << "   (.`\\@@/  / (|)' '(|) \\" << endl;
+    cout << "  *'@@  /|''\\--,:___:,--/" << endl;
+    cout << "/ @,--~/|(~~`\\_ V   V _/" << endl;
+    cout << "|\"---< | \\    \\\\ ___ //" << endl;
+    cout << "|>----<\\ @)    `;~~~;`" << endl;
+    cout << "\\(>-----<\\@\")" << endl;
 }
 
 void displayMap(Stage* stage) {
     system("cls");
     char condition[7];
     for (int i = 0; i < 7; i++) {
-        if (i == 6 && stage[i].isStageEmpty()){
+        if (i == 6 && stage[i].isStageEmpty()) {
             condition[i] = '@';
-        }else if (stage[i].isStageEmpty()) {
+        }
+        else if (stage[i].isStageEmpty()) {
             condition[i] = '#';
-        }else{
+        }
+        else {
             condition[i] = '^';
         }
     }
-    cout<<"           ("<<condition[0]<<")"<<endl;
-    cout<<"            |"<<endl;
-    cout<<"            |"<<endl;
-    cout<<"           ("<<condition[1]<<")"<<endl;
-    cout<<"            |"<<endl;
-    cout<<"            |"<<endl;
-    cout<<"           ("<<condition[2]<<")"<<endl;
-    cout<<"           /  \\_"<<endl;
-    cout<<"          /     \\_"<<endl;
-    cout<<"         /       ("<<condition[5]<<")"<<endl;
-    cout<<"("<<condition[3]<<")----("<<condition[4]<<")        |"<<endl;
-    cout<<"                 /"<<endl;
-    cout<<"                |"<<endl;
-    cout<<"                 \\"<<endl;
-    cout<<"                 ("<<condition[6]<<")"<<endl;
+    cout << "           (" << condition[0] << ")" << endl;
+    cout << "            |" << endl;
+    cout << "            |" << endl;
+    cout << "           (" << condition[1] << ")" << endl;
+    cout << "            |" << endl;
+    cout << "            |" << endl;
+    cout << "           (" << condition[2] << ")" << endl;
+    cout << "           /  \\_" << endl;
+    cout << "          /     \\_" << endl;
+    cout << "         /       (" << condition[5] << ")" << endl;
+    cout << "(" << condition[3] << ")----(" << condition[4] << ")        |" << endl;
+    cout << "                 /" << endl;
+    cout << "                |" << endl;
+    cout << "                 \\" << endl;
+    cout << "                 (" << condition[6] << ")" << endl;
 }
 
 void display(Stage stage) {
@@ -166,6 +181,15 @@ void display(Stage stage) {
 }
 int main() {
     Stage* dungeon = createDungeon();
+    while (true){
+        displayMap(&dungeon[0]);
+        
+        cout<<endl;system("pause");
+    }
     
     displayMap(&dungeon[0]);
+
+    // phoneixModel();
+    // beastBearModel();
+    // levithanModel();
 }
